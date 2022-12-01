e5f6a5857232ca9db3060836e354dba8b8ad31db
Desainer Gratis <mathscript787@gmail.com>
2022-12-01 19:54:05 +0700

    Fibonacci nested recursive still don't understand

---
+++ src/com/basicjava/recursiveFunction.java
@@ -0,0 +1,31 @@
+package src.com.basicjava;
+
+public class recursiveFunction {
+
+
+    //  private static int recFunction(int x) {
+    //     System.out.println("Value before -1: "+x);
+
+    //     if (x == 0) {
+    //         return x;
+    //     }
+        
+    //     return x + recFunction(x-1);
+        
+    // }
+
+    private static int fibbonacciRecursive(int x, String daun){
+            System.out.println("Daun : "+daun);
+            System.out.println("X : "+x);
+            if(x == 1 || x == 0) {
+                return x;
+            }
+            
+            return fibbonacciRecursive(x-1,"Kiri") + fibbonacciRecursive(x-2,"Kanan");
+
+    }
+    public static void main(String[] args) {
+        int resultRecursive = fibbonacciRecursive(5,"daun");
+            System.out.print("Fibbonacci ke : "+ resultRecursive);
+    }
+}
